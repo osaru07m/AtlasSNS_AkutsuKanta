9ddec1d4f61f2e2eee7280eb727281728e48dab2
おさる <osaru07m@gmail.com>
2025-07-14 19:00:50 +0900

    feat: ユーザーの新規登録完了ページに登録したユーザー名を表示させる

--- resources/views/auth/added.blade.php
+++ resources/views/auth/added.blade.php
@@ -1,10 +1,13 @@
 <x-logout-layout>
   <div id="clear">
-    <p>〇〇さん</p>
-    <p>ようこそ！AtlasSNSへ！</p>
-    <p>ユーザー登録が完了しました。</p>
-    <p>早速ログインをしてみましょう。</p>
-
+    <h2>
+      <span>{{ session('username') }}さん</span>
+      <span>ようこそ！AtlasSNSへ！</span>
+    </h2>
+    <div>
+      <p>ユーザー登録が完了いたしました。</p>
+      <p>早速ログインをしてみましょう！</p>
+    </div>
     <p class="btn"><a href="login">ログイン画面へ</a></p>
   </div>
 </x-logout-layout>
