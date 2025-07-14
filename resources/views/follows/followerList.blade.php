9389517d3f8354e26670ed0393efb56fa740ca37
おさる <osaru07m@gmail.com>
2025-07-15 02:06:12 +0900

    feat: ユーザーアイコンに相手ユーザーのプロフィールページへ遷移するリンクを設置する

--- resources/views/follows/followerList.blade.php
+++ resources/views/follows/followerList.blade.php
@@ -4,7 +4,9 @@
     <h2>フォロワーリスト</h2>
     <div id="followerList">
       @foreach (Auth::user()->followers as $user)
-      <img src="{{ asset('images/' . $user->icon_image) }}" alt="{{ $user->username }}さんのアイコン">
+      <a href="{{ route('profile.other', ['userId' => $user->id]) }}">
+        <img src="{{ asset('images/' . $user->icon_image) }}" alt="{{ $user->username }}さんのアイコン">
+      </a>
       @endforeach
     </div>
   </div>
