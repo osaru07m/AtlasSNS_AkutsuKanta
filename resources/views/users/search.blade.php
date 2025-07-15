1974bde9c7c6f8655a207995100270ee26a9622f
おさる <osaru07m@gmail.com>
2025-07-15 22:49:48 +0900

    feat: 検索ワードの入力フォームを設置する

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -1,4 +1,13 @@
 <x-login-layout>
 
+    <div id="searchContainer">
+        {{ Form::open(['url' => route('search')]) }}
+            <div>
+                {{ Form::text('keyword', old('keyword'), ['class' => 'input']) }}
+                <button type="submit" class="search-button">
+                    <img src="{{ asset('images/search.png') }}" alt="検索" style="width: 24px; height: 24px;">
+                </button>
+            </div>
+        {{ Form::close() }}
 
 </x-login-layout>
