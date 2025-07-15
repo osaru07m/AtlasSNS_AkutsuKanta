aa3ed119112b33c21293f56532727daf146fdb70
おさる <osaru07m@gmail.com>
2025-07-15 13:50:31 +0900

    fix: header内ロゴをasset関数を使用するようにする

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,5 @@
         <div id="head">
-            <h1><a href="{{ route('top') }}"><img src="images/atlas.png"></a></h1>
+            <h1><a href="{{ route('top') }}"><img src="{{ asset('images/atlas.png') }}"></a></h1>
             <div id="headNav">
                 <div id="headNavToggler">
                     <p>〇〇さん</p>
