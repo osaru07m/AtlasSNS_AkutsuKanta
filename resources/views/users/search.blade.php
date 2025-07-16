80636aaee43a792515f3d0ed7d17ea3d318beed9
おさる <osaru07m@gmail.com>
2025-07-16 14:54:54 +0900

    feat: フォローする/フォロー解除ボタンを画面に設置する

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -15,6 +15,26 @@
         <ul id="users">
             @foreach ($users as $user)
             {{ $user->username }}
+            <li>
+                @php
+                $auth = Auth::user();
+                $isFollowing = $auth->isFollowing($user->id);
+                $isFollowedBy = $auth->isFollowedBy($user->id);
+                @endphp
+
+                <div>
+                    @if ($isFollowedBy)
+                    <p class="is-follower">フォローされています</p>
+                    @endif
+
+                    {{-- ボタン分岐 --}}
+                    @if (!$isFollowing)
+                    <button class="btn follow">フォローする</button>
+                    @elseif ($isFollowing)
+                    <button class="btn unfollow">フォロー解除</a>
+                    @endif
+                </div>
+            </li>
             @endforeach
         </ul>
     </div>
