9db5237ffe74a6afd0319642bd697d9a58a77fdf
おさる <osaru07m@gmail.com>
2025-07-16 23:29:40 +0900

    fix: ユーザー検索画面にてAuthユーザーを表示しないように修正する

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
@@ -13,12 +14,12 @@ class UsersController extends Controller
         $keyword = $request->input('keyword');
 
         if(!is_null($keyword)) {
-            $users = User::where('username', 'like', "%{$keyword}%")->get();
+            $users = User::where('username', 'like', "%{$keyword}%")->where('id', '!=', Auth::user()->id)->get();
 
             return view('users.search', compact('keyword', 'users'));
         }
         else {
-            $users = User::get();
+            $users = User::where('id', '!=', Auth::user()->id)->get();
 
             return view('users.search', compact('users'));
         }
