b65291c7563aa36bec552b1cb947a8a33dc42061
おさる <osaru07m@gmail.com>
2025-07-17 01:39:24 +0900

    feat: 投稿一覧のスタイルを作成する

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,27 +3,32 @@
   <div id="postContainer">
     <form action="{{ route('top') }}" method="POST">
       @csrf
-      <img src="{{ asset('images/' . Auth::user()->icon_image) }}" alt="{{ Auth::user()->username }}のアイコン">
+      <div class="icon">
+        <img src="{{ asset('images/' . Auth::user()->icon_image) }}" alt="{{ Auth::user()->username }}のアイコン">
+      </div>
       <textarea name="post" placeholder="投稿内容を入力してください。" required maxlength="150"></textarea>
-      <button>
-        <img src="{{ asset('images/post.png') }}" alt="投稿する">
-      </button>
+      <div class="btnBox">
+        <button class="btn">
+          <img src="{{ asset('images/post.png') }}" alt="投稿する">
+        </button>
+      </div>
     </form>
-  </div>
 
   {{-- {{ dd($posts) }} --}}
 
-  <ul id="postList">
-  @foreach ($posts as $post)
-  <li>
-    <img src="{{ asset('images/' . $post->user->icon_image) }}" alt="{{ $post->user->username }}さんのアイコン">
-    <div>
-      <span>{{ $post->user->username }}</span>
-      <p>{{ $post->post }}</p>
-    </div>
-    <span>{{ $post->created_at->format('Y-m-d H:i') }}</span>
-  </li>
-  @endforeach
-  </ul>
+    <ul id="postList">
+    @foreach ($posts as $post)
+    <li>
+      <img src="{{ asset('images/' . $post->user->icon_image) }}" alt="{{ $post->user->username }}さんのアイコン">
+      <div>
+        <span>{{ $post->user->username }}</span>
+        <p>{{ $post->post }}</p>
+      </div>
+      <span>{{ $post->created_at->format('Y-m-d H:i') }}</span>
+    </li>
+    @endforeach
+    </ul>
+
+  </div>
 
 </x-login-layout>
